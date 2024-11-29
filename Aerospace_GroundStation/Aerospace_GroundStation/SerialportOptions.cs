a7bed4fefd76c5fb5e2493b39a3f4dc27c762646
Ismailsevinc <233301154@ogr.selcuk.edu.tr>
2024-11-29 23:14:33 +0300

    Efeler Aerospace Ground Station

---
+++ Aerospace_GroundStation/Aerospace_GroundStation/SerialportOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Aerospace_GroundStation
+{
+    public static class SerialportConfigure//seri port ayarları yapmaya yarar
+    {
+        public static void SerialPortOptions(SerialPort serialport,System.Windows.Forms.ComboBox comboBoxSERİALPORT,System.Windows.Forms. ComboBox comboBoxBAUDRATE) // seri port ayarları
+        {
+            
+            serialport = new SerialPort();
+            serialport.PortName = comboBoxSERİALPORT.SelectedIndex.ToString();
+            serialport.BaudRate = comboBoxBAUDRATE.SelectedIndex;
+            serialport.DataBits = 8;
+            serialport.Parity = Parity.None;
+            serialport.StopBits = StopBits.None;
+        }
+
+    }
+}
---
+++ Aerospace_GroundStation/Aerospace_GroundStation/veriler/GonderilecekVeriler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aerospace_GroundStation.veriler
+{
+    public class GonderilecekVeriler
+    {
+        public string RHRH { get; set; }//rakam harf rakam harf kodu
+        public double IOTSicaklik { get; set; }
+        public int IOTNem { get; set; }
+    }
+}
